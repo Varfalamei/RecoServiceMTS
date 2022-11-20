c8882e0e23e4bb6e207ef6be622d0093d1bfc9ab
Shakirov Renat <renatshk@gmail.com>
2022-11-20 19:17:12 +0300

    first attempt to pass tests

--- service/api/views.py
+++ service/api/views.py
@@ -28,14 +28,9 @@ class NotFoundError(BaseModel):
 
 router = APIRouter()
 
-api_key_query = APIKeyQuery(
-    name=config_env["API_KEY_NAME"],
-    auto_error=False
-)
-api_key_header = APIKeyHeader(
-    name=config_env["API_KEY_NAME"],
-    auto_error=False
-)
+api_key_query = APIKeyQuery(name=config_env["API_KEY_NAME"], auto_error=False)
+api_key_header = APIKeyHeader(name=config_env["API_KEY_NAME"],
+                              auto_error=False)
 token_bearer = HTTPBearer(auto_error=False)
 
 
@@ -57,13 +52,9 @@ async def get_api_key(
 @router.get(
     path="/health",
     tags=["Health"],
-    responses={
-        404: {"model": NotFoundError, "user": NotFoundError},
-    }
+    responses={404: {"model": NotFoundError, "user": NotFoundError}},
 )
-async def health(
-    api_key: APIKey = Depends(get_api_key)
-) -> str:
+async def health(api_key: APIKey = Depends(get_api_key)) -> str:
     return "I am alive"
 
 
@@ -71,22 +62,20 @@ async def health(
     path="/reco/{model_name}/{user_id}",
     tags=["Recommendations"],
     response_model=RecoResponse,
-    responses={
-        404: {"model": NotFoundError, "user": NotFoundError},
-    }
+    responses={404: {"model": NotFoundError, "user": NotFoundError}, },
 )
 async def get_reco(
     request: Request,
     model_name: str,
     user_id: int,
-    api_key: APIKey = Depends(get_api_key)
+    api_key: APIKey = Depends(get_api_key),
 ) -> RecoResponse:
     app_logger.info(f"Request for model: {model_name}, user_id: {user_id}")
 
     if user_id > 10 ** 9:
         raise UserNotFoundError(error_message=f"User {user_id} not found")
 
-    if model_name != 'model_1':
+    if model_name != "model_1":
         raise ModelNotFoundError(error_message=f"Model {model_name} not found")
 
     k_recs = request.app.state.k_recs
