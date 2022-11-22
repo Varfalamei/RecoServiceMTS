b160c961f21bd088709f5c4f93732aa845849845
Shakirov Renat <renatshk@gmail.com>
2022-11-22 18:28:23 +0300

    Refactor tests

--- tests/api/test_views.py
+++ tests/api/test_views.py
@@ -3,15 +3,17 @@ from http import HTTPStatus
 from starlette.testclient import TestClient
 
 from service.settings import ServiceConfig
+from service.api.config import config_env
 
 GET_RECO_PATH = "/reco/{model_name}/{user_id}"
+headers = {config_env['API_KEY_NAME']: config_env["API_KEY"]}
 
 
 def test_health(
     client: TestClient,
 ) -> None:
     with client:
-        response = client.get("/health")
+        response = client.get("/health", headers=headers)
     assert response.status_code == HTTPStatus.OK
 
 
@@ -20,9 +22,9 @@ def test_get_reco_success(
     service_config: ServiceConfig,
 ) -> None:
     user_id = 123
-    path = GET_RECO_PATH.format(model_name="some_model", user_id=user_id)
+    path = GET_RECO_PATH.format(model_name="model_1", user_id=user_id)
     with client:
-        response = client.get(path)
+        response = client.get(path, headers=headers)
     assert response.status_code == HTTPStatus.OK
     response_json = response.json()
     assert response_json["user_id"] == user_id
@@ -33,10 +35,10 @@ def test_get_reco_success(
 def test_get_reco_for_unknown_user(
     client: TestClient,
 ) -> None:
-    user_id = 10**10
+    user_id = 10 ** 10
     path = GET_RECO_PATH.format(model_name="some_model", user_id=user_id)
     with client:
-        response = client.get(path)
+        response = client.get(path, headers=headers)
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json()["errors"][0]["error_key"] == "user_not_found"
 
@@ -48,6 +50,6 @@ def test_get_reco_for_unknown_model(
     model_name = 'model_2'
     path = GET_RECO_PATH.format(model_name=model_name, user_id=user_id)
     with client:
-        response = client.get(path)
+        response = client.get(path, headers=headers)
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json()["errors"][0]["error_key"] == "model_not_found"
