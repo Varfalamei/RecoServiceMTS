2e83d046262bd865d1ab9b61b5b2ef13d375ae6b
Vladislav Mostovik <56130198+Mostovik71@users.noreply.github.com>
2022-11-24 11:08:03 +0300

    Update models_zoo.py

--- service/api/models_zoo.py
+++ service/api/models_zoo.py
@@ -53,4 +53,17 @@ class TopPopularAllCovered(BaseModelZoo):
             11237, 8636, 14741
         ][:k_recs]
         return reco
-
+class Popular(BaseModelZoo):
+    def reco_predict(
+        self,
+        user_id: int,
+        k_recs: int
+    ) -> List[int]:
+        """
+        Main function for recommendation items to users
+        :param user_id: user identification
+        :param k_recs: how many recs do you need
+        :return: list of recommendation ids
+        """
+        reco = [10440, 15297, 9728, 13865, 4151, 3734, 2657, 4880, 142, 6809]
+        return reco
