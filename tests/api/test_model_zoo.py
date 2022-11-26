152415d86837a4cd10116197d68705115d726fad
Shakirov Renat <renatshk@gmail.com>
2022-11-27 00:17:31 +0300

    update test

--- tests/api/test_model_zoo.py
+++ tests/api/test_model_zoo.py
@@ -1,6 +1,6 @@
 from starlette.testclient import TestClient
 
-from service.api.models_zoo import BlendingKNN
+from service.api.models_zoo import KNNModelWithTop
 from service.settings import ServiceConfig
 
 
@@ -10,7 +10,7 @@ def test_base_funcs(
 ) -> None:
     user_id = 176549
     k_recs = 10
-    model = BlendingKNN()
+    model = KNNModelWithTop(path_to_reco="data/KNNBM25withAddFeatures.csv.gz")
     reco = model.reco_predict(user_id=user_id, k_recs=k_recs)
     assert len(reco) == service_config.k_recs
     assert all(isinstance(item_id, int) for item_id in reco)
